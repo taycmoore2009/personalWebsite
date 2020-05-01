3e00ed11317caf983b6b9c9997fdab6c72cf5968
taycmoore2009 <tay.moore@chromeriver.com>
2020-05-01 15:34:08 -0700

    Added code to authorization

--- src/components/coding/socialMediaAdmin/auth.js
+++ src/components/coding/socialMediaAdmin/auth.js
@@ -8,21 +8,25 @@ export default function() {
     const search = window.location.search.split("?")[1]
     const params = search ? search.split('&') : [];
     let code = '';
+    let wallCode = '';
 
     params.forEach((param) => {
         const pairs = param.split("=");
         if(pairs[0] === 'code') {
             code = pairs[1];
         }
+        if(pairs[0] === 'wallCode') {
+            wallCode = pairs[1];
+        }
     });
     if(code) {
-        const url = `https://dxk3dp2ts2.execute-api.us-east-2.amazonaws.com/personal/instaAuth?code=${code}`;
+        const url = `https://dxk3dp2ts2.execute-api.us-east-2.amazonaws.com/personal/instaAuth?code=${code}&wallCode=${wallCode}`;
         addCode(url)
             .then((resp) => {
-                window.close();
+                console.log(resp);
             })
             .catch((err) => {
-                window.close();
+                console.log(err);
             });
     }
     return (<div>Authorization Complete!</div>);
